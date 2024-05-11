a06175db2f0e7a9b95c91327db81cd028da2f06d
Caio Vinicius <caioviniciuscv18@gmail.com>
2024-05-11 19:27:56 -0300

    Initial commit

---
+++ sample_union.c
@@ -0,0 +1,27 @@
+//
+// Created by caiocv18 on 11/05/2024.
+//
+#include <stdio.h>
+
+union Dados {
+    int inteiro;
+    float flutuante;
+    char* texto;
+};
+
+int main() {
+    union Dados dados;
+
+    dados.inteiro = 42;
+    printf("Valor inteiro: %d\n", dados.inteiro);
+
+    dados.flutuante = 3.1415;
+    // Neste ponto, o valor de `inteiro` pode ser corrompido devido à nova atribuição a `flutuante`
+    printf("Valor flutuante: %f\n", dados.flutuante);
+
+    dados.texto = "Olá, mundo!";
+    // Neste ponto, os valores de `inteiro` e `flutuante` são indefinidos
+    printf("Valor texto: %s\n", dados.texto);
+
+    return 0;
+}
