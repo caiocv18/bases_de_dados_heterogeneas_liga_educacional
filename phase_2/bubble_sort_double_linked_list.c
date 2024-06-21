a57c6c8f7d02bf8f3eb439825d0a1d7e643add9c
Caio Vinicius <caioviniciuscv18@gmail.com>
2024-06-21 17:31:21 -0300

    Implementing Bubble Sort for a doubly linked list

--- phase_2/bubble_sort_double_linked_list.c
+++ phase_2/bubble_sort_double_linked_list.c
@@ -1,25 +1,25 @@
 //
 // Created by caiocv18 on 21/06/2024.
 //
-
 #include <stdio.h>
 #include <stdlib.h>
 
-// Estrutura para um nodo de lista ligada
+// Estrutura para um nodo de lista duplamente ligada
 struct Node {
     int data;
     struct Node* next;
+    struct Node* prev;
 };
 
 // Função para trocar dados de dois nodos a e b
-void swap(struct Node *a, struct Node *b) {
+void swap2(struct Node *a, struct Node *b) {
     int temp = a->data;
     a->data = b->data;
     b->data = temp;
 }
 
-// Função para aplicar bubble sort na lista ligada
-void bubbleSort2(struct Node *start) {
+// Função para aplicar bubble sort na lista duplamente ligada
+void bubbleSort3(struct Node *start) {
     int swapped;
     struct Node *ptr1;
     struct Node *lptr = NULL;
@@ -34,7 +34,7 @@ void bubbleSort2(struct Node *start) {
 
         while (ptr1->next != lptr) {
             if (ptr1->data > ptr1->next->data) {
-                swap(ptr1, ptr1->next);
+                swap2(ptr1, ptr1->next);
                 swapped = 1;
             }
             ptr1 = ptr1->next;
@@ -44,8 +44,8 @@ void bubbleSort2(struct Node *start) {
     while (swapped);
 }
 
-// Função para imprimir os nodos da lista ligada
-void printList2(struct Node *start) {
+// Função para imprimir os nodos da lista duplamente ligada
+void printList3(struct Node *start) {
     struct Node *temp = start;
     printf("\n");
     while (temp!=NULL) {
@@ -55,16 +55,17 @@ void printList2(struct Node *start) {
 }
 
 // Função para criar um novo nodo
-struct Node *newNode2(int data) {
+struct Node *newNode3(int data) {
     struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
     new_node->data = data;
     new_node->next = NULL;
+    new_node->prev = NULL;
     return new_node;
 }
 
 // Função para adicionar um novo nodo no final da lista
-void insertAtEnd(struct Node** start, int data) {
-    struct Node *new_node = newNode2(data);
+void insertAtEnd3(struct Node** start, int data) {
+    struct Node *new_node = newNode3(data);
     struct Node *last = *start;
 
     if(*start == NULL) {
@@ -76,24 +77,25 @@ void insertAtEnd(struct Node** start, int data) {
         last = last->next;
 
     last->next = new_node;
+    new_node->prev = last;
 }
 
-int main_bubble_sort_linked_list() {
-    struct Node *start = newNode2(64);
-    insertAtEnd(&start, 34);
-    insertAtEnd(&start, 25);
-    insertAtEnd(&start, 12);
-    insertAtEnd(&start, 22);
-    insertAtEnd(&start, 11);
-    insertAtEnd(&start, 90);
+int main() {
+    struct Node *start = newNode3(64);
+    insertAtEnd3(&start, 34);
+    insertAtEnd3(&start, 25);
+    insertAtEnd3(&start, 12);
+    insertAtEnd3(&start, 22);
+    insertAtEnd3(&start, 11);
+    insertAtEnd3(&start, 90);
 
-    printf("Lista ligada antes de ser ordenada ");
-    printList2(start);
+    printf("Lista duplamente ligada antes de ser ordenada ");
+    printList3(start);
 
-    bubbleSort2(start);
+    bubbleSort3(start);
 
-    printf("\n\nLista ligada depois de ser ordenada ");
-    printList2(start);
+    printf("\n\nLista duplamente ligada depois de ser ordenada ");
+    printList3(start);
 
     return 0;
 }
