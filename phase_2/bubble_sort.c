86cffda52f170c6e14c848855ddfee46f90ff716
Caio Vinicius <caioviniciuscv18@gmail.com>
2024-06-21 17:14:33 -0300

    Implementing Bubble Sort for Linked List

---
+++ phase_2/bubble_sort.c
@@ -0,0 +1,40 @@
+//
+// Created by caiocv18 on 21/06/2024.
+//
+#include <stdio.h>
+
+// Função para realizar a ordenação usando o algoritmo Bubble Sort
+void bubbleSort(int arr[], int n) {
+   int i, j, temp;
+
+   // Loop externo para passar por todos os elementos
+   for(i = 0; i < n-1; i++) {
+      // Loop interno para realizar a comparação entre elementos adjacentes
+      for(j = 0; j < n-i-1; j++) {
+         // Troca os elementos se estiverem na ordem errada
+         if(arr[j] > arr[j+1]) {
+            temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+         }
+      }
+   }
+}
+
+int main() {
+   // Inicializa um array com elementos desordenados
+   int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+   // Calcula o tamanho do array
+   int n = sizeof(arr) / sizeof(arr[0]);
+
+   // Chama a função bubbleSort para ordenar o array
+   bubbleSort(arr, n);
+
+   // Imprime o array ordenado
+   printf("Array ordenado: \n");
+   for(int i = 0; i < n; i++)
+      printf("%d ", arr[i]);
+   printf("\n"); // Adiciona uma nova linha após a impressão do array
+
+   return 0;
+}
