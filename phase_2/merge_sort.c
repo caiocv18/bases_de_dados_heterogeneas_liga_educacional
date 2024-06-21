871d0adc3bdd3a315646fc01d169f44f52d26372
Caio Vinicius <caioviniciuscv18@gmail.com>
2024-06-21 17:35:48 -0300

    Implementing Selection Sort

--- phase_2/merge_sort.c
+++ phase_2/merge_sort.c
@@ -21,7 +21,7 @@ void selectionSort(int arr[], int n) {
     }
 }
 
-int main() {
+int main_selection() {
     int arr[] = {64, 25, 12, 22, 11};
     int n = sizeof(arr)/sizeof(arr[0]);
     selectionSort(arr, n);
